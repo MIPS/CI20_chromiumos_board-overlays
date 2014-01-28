5d8aa94cda2898c4961cf5446938dd4eb92a9506
Ben Zhang <benzh@chromium.org>
2014-01-28 14:03:44 -0800

    Add a new kparam for iTCO watchdog timer to disable TCO SMI on existing devices
    Vendor support 3 is selected on x86/x86_64 Chrome OS to disable
    TCO SMI. This vendor support option is a kernel workaround to
    make the iTCO watchdog timer work on existing devices with
    older firmware.
    
    See drivers/watchdog/iTCO_vendor_support.c for more info about the workaround.
    See the bug page for the related firmware changes.
    
    BUG=chromium:321832
    TEST=platform_HWwatchdog on mario,zgb,alex,parrot,stumpy,lumpy,butterfly,stout,link,falco,peppy,leon
    
    Change-Id: Id52c1f6aef9a176204209cca8cebd404f02cb7fb
    Signed-off-by: Ben Zhang <benzh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/184168
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ overlay-lumpy/scripts/build_kernel_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+modify_kernel_command_line() {
+  echo "iTCO_vendor_support.vendorsupport=3" >> "$1"
+}
