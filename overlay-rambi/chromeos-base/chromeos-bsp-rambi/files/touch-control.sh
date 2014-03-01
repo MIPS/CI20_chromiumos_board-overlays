eb92e5b9b360c8ad59b9817575451df7ecabde79
Benson Leung <bleung@chromium.org>
2014-02-28 18:40:34 -0800

    rambi : Bring in touch-control script from link
    We used this optional script in the Link overlay to disable acpi wakeups
    for touchscreen and touchpad from the power manager. This provided
    an extra level of protection from spurious wakes.
    (see crbug.com/224255).
    
    Lets bring this back for Rambi, and have it unconditionally
    disable the touchscreen, in case the wakeup int is floating.
    
    Signed-off-by: Benson Leung <bleung@chromium.org>
    
    BUG=chrome-os-partner:27508
    TEST=Manually test touch script disables/enables the wake sources.
    1. /opt/google/touch/touch-control.sh --disable
    2. cat /proc/acpi/wakeup
    3. Check that the following two lines are there:
    TPAD	  S3	*disabled  pnp:00:0b
    TSCR	  S3	*disabled  pnp:00:0c
    4. /opt/google/touch/touch-control.sh --enable
    5. cat /proc/acpi/wakeup
    6. Check that the following two lines are there:
    TPAD	  S3	*enabled   pnp:00:0b
    TSCR	  S3	*disabled  pnp:00:0c
    
    Note that the TSCR should always be disabled.
    
    Change-Id: I6d147d70da034516d547dae73a6351bc1e3ef28a
    Reviewed-on: https://chromium-review.googlesource.com/192946
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Benson Leung <bleung@chromium.org>
    Commit-Queue: Benson Leung <bleung@chromium.org>

---
+++ overlay-rambi/chromeos-base/chromeos-bsp-rambi/files/touch-control.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+. /usr/share/misc/shflags
+
+DEFINE_boolean 'enable' ${FLAGS_FALSE} "Enable Touch Device Wakeup" e
+DEFINE_boolean 'disable' ${FLAGS_FALSE} "Disable Touch Device Wakeup" d
+
+# Parse command line
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
+
+ret=0
+for name in TPAD; do
+  if [ ${FLAGS_enable} -eq ${FLAGS_TRUE} ]; then
+    grep -q "${name}.*enabled" /proc/acpi/wakeup && continue
+    printf "${name}" > /proc/acpi/wakeup
+    grep -q "${name}.*enabled" /proc/acpi/wakeup && continue
+    ret=1
+    continue
+  fi
+
+  if [ ${FLAGS_disable} -eq ${FLAGS_TRUE} ]; then
+    grep -q "${name}.*disabled" /proc/acpi/wakeup && continue
+    printf "${name}" > /proc/acpi/wakeup
+    grep -q "${name}.*disabled" /proc/acpi/wakeup && continue
+    ret=1
+    continue
+  fi
+done
+
+# Unconditionally disable TSCR
+name='TSCR'
+grep -q "${name}.*disabled" /proc/acpi/wakeup && exit ${ret}
+printf "${name}" > /proc/acpi/wakeup
+grep -q "${name}.*disabled" /proc/acpi/wakeup && exit ${ret}
+ret=1
+
+exit ${ret}
