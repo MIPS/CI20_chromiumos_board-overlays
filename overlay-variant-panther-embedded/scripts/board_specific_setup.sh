31f57972795101503fed81aeb2e1afcbfcaeb4ba
Steve Fung <stevefung@chromium.org>
2014-05-05 13:25:07 -0700

    Add initial panther embedded variant overlay
    BUG=chromium:368283
    TEST=`./build_packages --board=panther_embedded` succeeds
    TEST=`./build_image --board=panther_embedded` succeeds
    CQ-DEPEND=CL:198589, CL:198198
    
    Change-Id: Ia338e8813c700882d41945a18fe3e8285fccaf35
    Reviewed-on: https://chromium-review.googlesource.com/198634
    Reviewed-by: Gaurav Shah <gauravsh@chromium.org>
    Tested-by: Steve Fung <stevefung@chromium.org>
    Commit-Queue: Steve Fung <stevefung@chromium.org>

---
+++ overlay-variant-panther-embedded/scripts/board_specific_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+skip_test_image_content=1
