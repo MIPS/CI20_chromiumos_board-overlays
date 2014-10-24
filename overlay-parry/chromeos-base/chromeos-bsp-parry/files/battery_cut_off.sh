36df79a32cd687e8b6f91fbbe07bcadf24322c88
James C Chen <james.c.chen@intel.com>
2014-10-24 12:12:59 +0800

    Parry: Make batterycutoff to follow reference board
    BUG=chrome-os-partner:32230
    TEST=none
    
    Change-Id: I5a5c89028d9a09caa034c7631cbb1ad0d96e3573
    Reviewed-on: https://chromium-review.googlesource.com/225305
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>
    Commit-Queue: JAMES C Chen <james.c.chen@intel.com>
    Tested-by: JAMES C Chen <james.c.chen@intel.com>

--- overlay-parry/chromeos-base/chromeos-bsp-parry/files/battery_cut_off.sh
+++ overlay-parry/chromeos-base/chromeos-bsp-parry/files/battery_cut_off.sh
@@ -11,19 +11,16 @@ IMG_PATH="/usr/share/factory/images"
 TTY="/dev/tty1"
 
 modprobe i2c_dev
-
-/usr/sbin/board_discharge_battery.sh
-
-if ! (ectool battery | grep -q AC_PRESENT); then
-  if [ -e "${IMG_PATH}/connect_ac_batterycutoff.png" ]; then
-    ply-image --clear 0x000000 "${IMG_PATH}/connect_ac_batterycutoff.png"
+if (ectool battery | grep -q AC_PRESENT); then
+  if [ -e "${IMG_PATH}/remove_ac.png" ]; then
+    ply-image --clear 0x000000 "${IMG_PATH}/remove_ac.png"
   else
     echo "============================================" >"$TTY"
-    echo "========== Connect AC adapter now. =========" >"$TTY"
+    echo "========== Unplug AC adapter now. ==========" >"$TTY"
     echo "============================================" >"$TTY"
     echo "" >"$TTY"
   fi
-  while ! (ectool battery | grep -q AC_PRESENT) ; do
+  while (ectool battery | grep -q AC_PRESENT) ; do
     sleep 0.5;
   done
 fi
@@ -35,12 +32,8 @@ else
   echo "==== Cutting off battery. Wait 10 seconds. ====" >"$TTY"
   echo "===============================================" >"$TTY"
 fi
-
-if ectool batterycutoff; then
-  sleep 2
-  shutdown -h now
-  sleep 15
-fi
+ectool batterycutoff
+sleep 15
 
 # Couldn't have reached here
 if [ -e "${IMG_PATH}/cutoff_failed.png" ]; then
