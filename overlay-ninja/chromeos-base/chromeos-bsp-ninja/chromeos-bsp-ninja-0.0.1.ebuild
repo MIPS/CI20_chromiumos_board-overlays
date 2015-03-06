4430ad5c9a88c8a25a564422a4112ed31e2d47df
Huang, Huki <huki.huang@intel.com>
2015-03-06 10:04:34 +0800

    Ninja: Add public board overlay for ninja
    BUG=none
    TEST=none
    
    Change-Id: I947171312399c8739af7f098583dc646bb914018
    Reviewed-on: https://chromium-review.googlesource.com/256750
    Commit-Queue: Huki Huang <huki.huang@intel.com>
    Tested-by: Huki Huang <huki.huang@intel.com>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

--- overlay-ninja/chromeos-base/chromeos-bsp-ninja/chromeos-bsp-ninja-0.0.1.ebuild
+++ overlay-ninja/chromeos-base/chromeos-bsp-ninja/chromeos-bsp-ninja-0.0.1.ebuild
@@ -5,18 +5,24 @@ EAPI=4
 
 inherit appid
 
-DESCRIPTION="Ebuild which pulls in any necessary ebuilds as dependencies or portage actions"
+DESCRIPTION="Ninja bsp (meta package to pull in driver/tool deps)"
 
 LICENSE="BSD-Google"
 SLOT="0"
 KEYWORDS="-* amd64 x86"
+IUSE=""
 S="${WORKDIR}"
 
 RDEPEND="
+	chromeos-base/ec-utils
 	sys-kernel/linux-firmware
+	media-gfx/ply-image
 "
 DEPEND="${RDEPEND}"
+S="${WORKDIR}"
 
 src_install() {
 	doappid "{555B868F-306A-3E26-6687-FC081968D43A}"
+	dosbin "${FILESDIR}/board_factory_wipe.sh"
+	dosbin "${FILESDIR}/board_factory_reset.sh"
 }
---
+++ overlay-ninja/chromeos-base/chromeos-bsp-ninja/files/board_factory_wipe.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+#
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+/usr/sbin/activate_date --clean
+# make sure the cached vpd log file has been removed so that the the next reboot
+# will re-generate it in /etc/init/vpd-log.conf.
+VPD_2_0="/var/log/vpd_2.0.txt"
+rm -f $VPD_2_0
+sync
+sleep 3
+
+# this script is called by clobber-state
+for WIPE_OPTION in "$@"; do
+  if [ "$WIPE_OPTION" = "shutdown" ]; then
+    # shutdown after factory wipe-out
+    /sbin/shutdown -h now
+    # It is regarded as shutdown failure that it isn't shut down within 5 mins.
+    sleep 5m
+    echo "The device isn't shutdown correctly."
+    exit 1
+  fi
+done
+# reboot after return to clobber-state(default)
