0b48afbfd13213351c20c442109dace2487ae93c
Eric Yang <eric.yang@intel.com>
2015-05-18 17:40:33 +0800

    Orco: remove 'ply-image' and use 'display_boot_message' for Freon
    'ply-image' is no longer supported in Freon-based images.
    Switch to 'display_boot_message' to display wiping messages.
    
    BUG=chrome-os-partner:40242
    TEST=Factory wipe and battery cutoff, check the output messages.
    
    Change-Id: If0201916929850a730a36cf4debbafbd58db7985
    Reviewed-on: https://chromium-review.googlesource.com/271750
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Tested-by: Eric Yang <eric.yang@intel.com>
    Commit-Queue: Eric Yang <eric.yang@intel.com>

--- overlay-orco/chromeos-base/chromeos-bsp-orco/chromeos-bsp-orco-0.0.1.ebuild
+++ overlay-orco/chromeos-base/chromeos-bsp-orco/chromeos-bsp-orco-0.0.1.ebuild
@@ -16,7 +16,6 @@ S="${WORKDIR}"
 RDEPEND="
 	chromeos-base/ec-utils
 	sys-kernel/linux-firmware
-	media-gfx/ply-image
 "
 DEPEND="${RDEPEND}"
 S="${WORKDIR}"
@@ -30,9 +29,6 @@ src_install() {
 
 	# Wiping scripts
 	dosbin "${FILESDIR}"/sbin/*.sh
-
-	insinto "/usr/share/factory/images"
-	doins "${FILESDIR}"/images/*.png
 
 	# Install Bluetooth ID override.
 	insinto "/etc/bluetooth"
---
+++ overlay-orco/chromeos-base/chromeos-bsp-orco/files/sbin/display_wipe_message.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+#
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Script used to show boot message.
+
+FONT_SIZE="60"
+FONT_COLOR="Green"
+
+# Temp message file for display_boot_message.
+MESSAGE_FILE="$(mktemp --tmpdir)"
+
+on_exit() {
+  rm -f "${MESSAGE_FILE}"
+}
+
+# Prints usage help for commands usage.
+usage_help() {
+  echo "Usage: $0 mode [arg ...]
+
+  connect_ac: Message for connecting AC.
+
+  remove_ac: Message for removing AC.
+
+  charging: Message when charging battery.
+            Arg #1: the current battery level.
+            Arg #2: the minimum battery level to charge.
+
+  cutting_off: Message when running cut off commands.
+
+  cutoff_failed: Message when cut off failed.
+"
+}
+
+prepare_message() {
+  local message="
+    <span font=\"Noto Sans UI ${FONT_SIZE}\"
+          foreground=\"${FONT_COLOR}\">"
+
+  printf "${message}\n"
+  # Append messages with newline.
+  for message in "$@"; do
+    printf "${message}\n"
+  done
+  printf "</span>"
+}
+
+display_message() {
+  prepare_message "$@" >"${MESSAGE_FILE}"
+  display_boot_message "show_file" "${MESSAGE_FILE}"
+}
+
+mode_connect_ac() {
+  (FONT_COLOR="Red" && display_message "Battery level too low" \
+                                       "Please connect AC power" \
+                                       "电池电量过低" \
+                                       "请连接AC电源")
+}
+
+mode_remove_ac() {
+  (FONT_COLOR="Red" && display_message "Please remove AC power" \
+                                       "请移除AC电源")
+}
+
+mode_charging() {
+  local current_level="$1"
+  local min_level="$2"
+
+  display_message "Charging battery to ${min_level}%%..." \
+                  "Current level: ${current_level}%%" \
+                  "正在充电至${min_level}%%..." \
+                  "当前电量：${current_level}%%"
+}
+
+mode_cutting_off() {
+  display_message "Cutting off battery" \
+                  "Please wait..." \
+                  "切断电池电源中" \
+                  "请稍候..."
+}
+
+mode_cutoff_failed() {
+  (FONT_COLOR="Red" && display_message "Battery cut-off failed" \
+                                       "Please contact factory team" \
+                                       "无法切断电池电源" \
+                                       "请联络RD")
+}
+
+main() {
+  if [ $# -lt 1 ]; then
+    usage_help
+    exit 1
+  fi
+  local mode="$1"
+  shift
+
+  case "${mode}" in
+    "connect_ac" | "remove_ac" | "charging" | "cutting_off" | "cutoff_failed" )
+      mode_"${mode}" "$@"
+      ;;
+    * )
+      usage_help
+      exit 1
+      ;;
+  esac
+}
+
+trap on_exit EXIT
+main "$@"
