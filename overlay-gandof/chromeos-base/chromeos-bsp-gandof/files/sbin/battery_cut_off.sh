3cce8f5da468f224d08f333fc6b9480fb62eae52
Grace Kao <grace.kao@intel.com>
2015-05-19 11:15:44 +0800

    Gandof: remove 'ply-image' and use 'display_boot_message' for Freon
    'ply-image' is no longer supported in Freon-based images.
    Switch to 'display_boot_message' to display wiping messages.
    
    BUG=chrome-os-partner:40077
    TEST=Factory wipe and battery cutoff, check the output messages.
    
    Change-Id: Ic6c2ccfff3f5607ade747f28c69525523164ac3e
    Signed-off-by: Grace Kao <grace.kao@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/272013
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>

--- overlay-gandof/chromeos-base/chromeos-bsp-gandof/files/sbin/battery_cut_off.sh
+++ overlay-gandof/chromeos-base/chromeos-bsp-gandof/files/sbin/battery_cut_off.sh
@@ -7,45 +7,23 @@
 # This script uses ectool to send command to EC to cut off the battery power.
 #
 
-IMG_PATH="/usr/share/factory/images"
-TTY="/dev/tty1"
-
+DISPLAY_MESSAGE="/usr/sbin/display_wipe_message.sh"
 modprobe i2c_dev
+
 if (ectool battery | grep -q AC_PRESENT); then
-  if [ -e "${IMG_PATH}/remove_ac.png" ]; then
-    ply-image --clear 0x000000 "${IMG_PATH}/remove_ac.png"
-  else
-    echo "============================================" >"$TTY"
-    echo "========== Unplug AC adapter now. ==========" >"$TTY"
-    echo "============================================" >"$TTY"
-    echo "" >"$TTY"
-  fi
+  "${DISPLAY_MESSAGE}" "remove_ac"
   while (ectool battery | grep -q AC_PRESENT) ; do
     sleep 0.5;
   done
 fi
 
-if [ -e "${IMG_PATH}/cutting_off.png" ]; then
-  ply-image --clear 0x000000 "${IMG_PATH}/cutting_off.png"
-else
-  echo "===============================================" >"$TTY"
-  echo "==== Cutting off battery. Wait 10 seconds. ====" >"$TTY"
-  echo "===============================================" >"$TTY"
-fi
+"${DISPLAY_MESSAGE}" "cutting_off"
 
 ectool batterycutoff at-shutdown
 shutdown -h now
 sleep 15
 
-# Couldn't have reached here
-if [ -e "${IMG_PATH}/cutoff_failed.png" ]; then
-  ply-image --clear 0x000000 "${IMG_PATH}/cutoff_failed.png"
-else
-  echo "======================================" >"$TTY"
-  echo "========== Cut off failed!! ==========" >"$TTY"
-  echo "======================================" >"$TTY"
-  echo "" >"$TTY"
-fi
+"${DISPLAY_MESSAGE}" "cutoff_failed"
 
 sleep 1d
 exit 1
